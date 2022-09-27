ee03704202b4f2052796311a4d1e9d0b7e63310b
pickytruco <jordiborrasroca@gmail.com>
2022-09-27 17:44:16 +0200

    3

--- src/main/java/org/iesch/ad/Main.java
+++ src/main/java/org/iesch/ad/Main.java
@@ -1,10 +1,18 @@
 package org.iesch.ad;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
 
     public static void main(String[] args) {
 
+    String prueba = "Tengo una muñeca vestida de azul";
 
+    //StringUtils.substringAfter("abcba", 'b') = "cba"
+    String resultado = StringUtils.substringAfter(prueba,"muñeca");
+    String resultado2 = StringUtils.substringBefore(prueba, "muñeca");
+    System.out.println(resultado);
+    System.out.println(resultado2);
 
     }
 
